7c513be83a0a4791830056eaeb0e249c4d7ad51d
Aadi Malik <adnanjunaid76279@gmail.com>
2026-01-02 10:19:25 +0500

    update company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -108,7 +108,7 @@ class CompanyController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:companies,email,' . $id,
             'phone' => 'required|string|max:20',
-            'company_id' => 'required|integer|max:20',
+            'company_id' => 'required|integer',
         ]);
 
         if ($validator->fails()) {
